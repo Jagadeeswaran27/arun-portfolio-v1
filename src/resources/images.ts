b360bc96ed52ea833552bbd43872944372536930
Jagadeeswaran27 <jagadeeswaran2705@gmail.com>
2025-11-11 12:10:46 +0530

    content changed

--- src/resources/images.ts
+++ src/resources/images.ts
@@ -1,35 +1,45 @@
 import webp1 from "../assets/images/1.webp";
 import webp2 from "../assets/images/2.webp";
-import webp3 from "../assets/images/3.webp";
 import webp4 from "../assets/images/4.webp";
 import webp5 from "../assets/images/5.webp";
 import webp6 from "../assets/images/6.webp";
 import webp7 from "../assets/images/7.webp";
 import webp8 from "../assets/images/8.webp";
 import webp9 from "../assets/images/9.webp";
+import webp10 from "../assets/images/10.webp";
+import webp11 from "../assets/images/11.webp";
+import webp12 from "../assets/images/12.webp";
+import webp14 from "../assets/images/14.webp";
+import webp15 from "../assets/images/15.webp";
+import webp16 from "../assets/images/16.webp";
 import webp17 from "../assets/images/17.webp";
 import webp18 from "../assets/images/18.webp";
 import webp19 from "../assets/images/19.webp";
-import webp20 from "../assets/images/20.webp";
 import webp21 from "../assets/images/21.webp";
 import webp22 from "../assets/images/22.webp";
 import webp23 from "../assets/images/23.webp";
+import webp24 from "../assets/images/24.webp";
 
 export {
   webp1,
   webp2,
-  webp3,
   webp4,
   webp5,
   webp6,
   webp7,
   webp8,
   webp9,
+  webp10,
+  webp11,
+  webp12,
+  webp14,
+  webp15,
+  webp16,
   webp17,
   webp18,
   webp19,
-  webp20,
   webp21,
   webp22,
   webp23,
+  webp24,
 };
